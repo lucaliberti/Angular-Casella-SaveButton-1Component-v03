da62dea0b05bf826b0c391fa1d089c299721a881
lucaliberti <liberti.luca@gmail.com>
2022-02-13 22:11:42 +0100

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef, ViewChild  } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'tile';
+
+  testo_salvato:string="(ancora nulla)"
+  @ViewChild('inputCasella') htmlInputCasella: ElementRef;  
+  constructor() {}
+
+
+  onClick3() {
+    console.log(this.htmlInputCasella)
+    this.testo_salvato=this.htmlInputCasella.nativeElement.value;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+ /*
+  onClick1(htmlInput:HTMLInputElement) {
+    console.log(htmlInput)
+    this.testo_salvato=htmlInput.value 
+  }
+
+  onClick2( val:string) {
+    console.log(val)
+    this.testo_salvato=val 
+  }
+  */
+
+
+
+
 }
+
+
